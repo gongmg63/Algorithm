722fc26b6294bb8aa79fcfe2827964a42ebb0f22
mkong <rhdalsrn2@naver.com>
2024-10-30 21:21:22 +0900

    [ADD] x0C 백트래킹

---
+++ cote/untitled/src/x0C/N과M_6.java
@@ -0,0 +1,62 @@
+package x0C;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N과M_6 {
+    static boolean[] isVisited = new boolean[8];
+    static int[] arr = new int[8];
+    static int[] retArr = new int[8];
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static boolean checkAscending(int m) {
+        for (int i = 1; i < m; ++i)
+            if (retArr[i] < retArr[i - 1])
+                return false;
+        return true;
+    }
+
+    static void backTracking(int n, int m, int depth) throws IOException{
+        if (depth == m) {
+            if (checkAscending(m)) {
+                for (int i = 0; i < m; ++i)
+                    bw.write(retArr[i] + " ");
+                bw.write("\n");
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (!isVisited[i]) {
+                retArr[depth] = arr[i];
+                isVisited[i] = true;
+                backTracking(n, m, depth + 1);
+                isVisited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < 8; ++i)
+            arr[i] = Integer.MAX_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        backTracking(n, m, 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
